273defed888e0d66e58a02070816d883186bd270
linkme21 <linkmy67@gmail.com>
2024-06-11 22:27:28 +0300

    v1
    add first task

---
+++ taskat/session_1_task/main.php
@@ -0,0 +1,70 @@
+
+<?php
+
+
+
+
+echo phpversion();
+
+echo "<h1>Mazen Abdelnaser</h1>";
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        $myname = "mazen21";
+        echo "$myname kkkkk";
+        echo '<br>';
+        echo '$myname kkkkk';
+        echo '<br>';    
+    print "Mazen ". "Abdelnaser" ; 
+            echo 'ahmed\'s car';
+    ?>
+    <?php echo "<h1>Mazen Abdelnaser</h1>";
+    ////////////////////
+    // $x = 2;
+
+    // $y = &$x;
+    // $x = 200;
+
+    // echo $y;
+    //////////////////
+    
+
+
+
+    // $x = 'mazen';
+    // $$x = "3333";
+    // echo "nam {$$x}";
+
+
+    $isConnected = true;
+    $isOffliane = false;
+
+var_dump($isConnected);
+var_dump($isOffliane);
+// print_r($isConnected);
+// print_r($isOffliane);
+
+
+
+
+
+
+
+
+    
+    ?>
+    <h1>Mazen Abdelnaser</h1>
+</body>
+</html>
