1ef0186d0124c04f78a904e7eb66d6365dcd5829
Mazen <mazen.nasir@webenia.com>
2024-07-16 18:02:45 +0300

    task5

---
+++ taskat/session_2_task/assets/footer.php
@@ -0,0 +1,82 @@
+
+
+
+            <!--== Start Footer Main ==-->
+            <div class="footer-main pb-3 pt-4">
+                <div class="container">
+                    <div class="row border-bottom border-wait  justify-content-lg-between text-center justify-content-sm-aruwnd">
+                        <div class="col-lg-6 col-md-6 text-lg-start col-12">
+                            <h3 class="hero-slide-title">find us there:</h3>
+                        <img src="assets/images/icons/Group 154.png" class=" w-50" alt="">
+                        </div>
+                        <div class="col-lg-6 col-md-6 col-12 text-lg-end text-center">
+                            <h3 class="hero-slide-title">Payment methods:</h3>
+                            <img src="assets/images/icons/Group 151.png" class="w-50" alt="">
+                        </div>
+                    </div>
+                    
+                    <div class="row">
+                        <div class="col-md-6 col-lg-3">
+                            <div class="widget-item">
+                                <div class="widget-about">
+                                    <ul>
+                                        <h3>About</h3>
+                                        <li><a href="">About us</a></li>
+                                        <li><a href="">Categories</a></li>
+                                        <li><a href="">Shop</a></li>
+                                        <li><a href="">Blog</a></li>
+                                        <li><a href="">FAQ</a></li>
+                                        <li><a href="">Contacts</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-lg-3">
+                            <div class="widget-item">
+                                <div class="widget-about">
+                                    <ul>
+                                        <h3>Categories</h3>
+                                        <li><a href="">About us</a></li>
+                                        <li><a href="">Categories</a></li>
+                                        <li><a href="">Shop</a></li>
+                                        <li><a href="">Blog</a></li>
+                                        <li><a href="">FAQ</a></li>
+                                        <li><a href="">Contacts</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-lg-3">
+                            <div class="widget-item">
+                                <div class="widget-about">
+                                    <ul>
+                                        <h3>Useful links</h3>
+                                        <li><a href="">Careers</a></li>
+                                        <li><a href="">Privacy policy</a></li>
+                                        <li><a href="">Terms of use</a></li>
+                                        <li><a href="">Support</a></li>
+                                        <li><a href="">Shipping details</a></li>
+                                        <li><a href="">Information</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-lg-3 mt-lg-0 mt-6">
+                            <div class="widget-item">
+                                <h3 class="widget-title">Social Info</h3>
+                                <div class="widget-social">
+                                    <a href="https://twitter.com/" target="_blank" rel="noopener"><i class="fa fa-twitter"></i></a>
+                                    <a href="https://www.facebook.com/" target="_blank" rel="noopener"><i class="fa fa-facebook"></i></a>
+                                    <a href="https://www.pinterest.com/" target="_blank" rel="noopener"><i class="fa fa-pinterest-p"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--== End Footer Main ==-->
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ taskat/session_2_task/assets/headr.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html class="no-js" lang="ar" dir="ltr">
+
+<head lang='@(isArabic ? "ar" : "en")'>
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="./assets/images/favicon.webp">
+
+    <!-- CSS (Font, Vendor, Icon, Plugins & Style CSS files) -->
+
+    <!-- Font CSS -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    
+    <!-- font-awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+    integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- Vendor CSS (Bootstrap & Icon Font) -->
+    <link rel="stylesheet" href="./assets/css/vendor/bootstrap.min.css">
+
+    <!-- Plugins CSS (All Plugins Files) -->
+    <link rel="stylesheet" href="assets/css/plugins/swiper-bundle.min.css">
+    <link rel="stylesheet" href="assets/css/plugins/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/plugins/fancybox.min.css">
+    <link rel="stylesheet" href="assets/css/plugins/nice-select.css">
+
+    <!-- Style CSS -->
+    <link rel="stylesheet" href="./assets/css/style.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
